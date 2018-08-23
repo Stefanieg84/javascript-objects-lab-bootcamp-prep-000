fbd8c61881e750764cb62a558782526a02d24fe8
Stefanieg84 <stefanieg84@gmail.com>
2018-08-23 14:55:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  return Object.assign({}, object, key);
+  var newObject = Object.assign({}, object);
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
